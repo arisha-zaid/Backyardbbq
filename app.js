6871c5692a54939840ff41b874b5db0c738b305a
arisha-zaid <arishazaid2006@gmail.com>
2024-11-20 17:19:35 +0530

    home-section and footer

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 let menu=document.querySelector('.menu-bars');
+let nav=document.querySelector('.nav-bar');
 menu.onclick=()=>{
     console.log("add");
-    menu.classList.toggle('view');
+    nav.classList.toggle('view');
 }
+
+var swiper = new Swiper(".home-section", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+   loop:true,
+  });
