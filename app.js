73e553c0fec37322284cfceecbca9241b80005ed
arisha-zaid <arishazaid2006@gmail.com>
2024-11-24 15:30:37 +0530

    renamed images and food section styling

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ menu.onclick=()=>{
     console.log("add");
     nav.classList.toggle('view');
 }
-
+// home page slides
 var swiper = new Swiper(".home-section", {
     spaceBetween: 30,
     centeredSlides: true,
@@ -18,3 +18,42 @@ var swiper = new Swiper(".home-section", {
     },
    loop:true,
   });
+
+//offer cards slide
+  var swiper = new Swiper(".offer-zone", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+
+  // food card page slides
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+ loop:true,
+});
+
+// stars   fa-regular---->fa-solid
+const star=document.querySelectorAll('fa-regular','fa-star');
+star.onclick=()=>{
+  console.log(" clicked!");
+}
